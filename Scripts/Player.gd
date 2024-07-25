f2389100c934018d7f52ca28876b575694de70d8
lfmutton <lfmutton@gmail.com>
2024-07-25 19:06:03 -0300

    adding enemy

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 @export var SPEED = 50.0
 
 
+var mutex : Mutex
+var thread: Thread
 # screen size
 var screenSize = DisplayServer.screen_get_size()
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var totalTime = 0
+
 
 #running variables
 var direction : float
@@ -20,31 +23,41 @@ const MIN_JUMP_SPEED = -200.0
 
 var jumping_direction : float = 0
 var current_jump : float = MIN_JUMP_SPEED
+var where_to_jump : Vector2
+
+func _ready():
+	mutex = Mutex.new()
+	thread = Thread.new()
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
 	direction = Input.get_axis("move_left", "move_right")
 	
 	# Handle jump.
 	if Input.is_action_pressed("space") and is_on_floor():
 		velocity.x = 0
 		jump(delta, direction)
+		
 	else:
 		move(direction)
 	
 	move_and_slide()
-	
+
 
 func _input(event):
 	if event.is_action_released("space") and is_on_floor():
-		velocity.y = current_jump
-		velocity.x = jumping_direction
+		velocity += Vector2(jumping_direction, current_jump)
+		if velocity.x > 0:
+			$Sprite2D.flip_h = false
+		elif velocity.x < 0:
+			$Sprite2D.flip_h = true
 		current_jump = MIN_JUMP_SPEED
+		jumping_direction = 0
 		print(is_on_floor())
-	
+
+
 func move(direction):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -56,9 +69,9 @@ func move(direction):
 			$Sprite2D.flip_h = true
 	else:
 		velocity.x = 0
-		
+
 func jump(delta, direction):
-	jumping_direction = move_toward(jumping_direction, 1000*direction, delta*1000)
+	if direction > 0 or direction < 0:
+		jumping_direction = move_toward(jumping_direction, 1000*direction, delta*200)
 	current_jump = move_toward(current_jump, MAX_JUMP_VELOCITY, delta * JUMP_FORCE_INCREMENT)
-	print(current_jump)
-	
+	print(jumping_direction)
